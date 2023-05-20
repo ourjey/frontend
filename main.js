816c0da29df7fe8f71f15871906a09a49d078fdf
Arjay Pasaylo <arjaypasaylo1@gmail.com>
2023-05-20 18:01:44 +0800

    Added Axios Functionality to access OpenAI

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const axios = require('axios');
 
 const isDev = true;
 
@@ -8,25 +9,60 @@ const createWindow = () => {
     width: isDev ? 1200 : 600,
     height: 600,
     webPreferences: {
-        preload: path.join(__dirname, 'preload.js'),
-      },
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-  if (isDev){
+  if (isDev) {
     win.webContents.openDevTools();
   }
 
-  win.loadFile(path.join(__dirname,'./renderer/index.html'));
-}
+  win.loadFile(path.join(__dirname, "./renderer/index.html"));
+};
 
 app.whenReady().then(() => {
-  createWindow()
+  ipcMain.handle('axios.openAI', openAI);
+  
+  createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+async function openAI(event, sentence){
+  let result = null
+
+  await axios({
+    method: 'post',
+    url: 'https://api.openai.com/v1/completions',
+    data: {
+      model: "text-davinci-003",
+      prompt: "Product description:\n\n" + sentence,
+      temperature: 0.8,
+      max_tokens: 60,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 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'
+    }
+  }).then(function (response) {
+    result = response.data;
   })
+  .catch(function (error) {
+    result = error;
+  });
+
+  return result;
+}
